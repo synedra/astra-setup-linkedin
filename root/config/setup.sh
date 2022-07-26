13316959a5d2009ccfc2baa6c95a3dfb83f90545
synedra-datastax <77410784+synedra-datastax@users.noreply.github.com>
2022-07-26 12:45:08 -0700

    Typo in astra-setup.

--- root/config/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cp -r /workspace/appsembler-tiktok/root/config/.bashrc /home/gitpod/.bashrc.d/999-datastax
-cp -r /workspace/appsembler-tiktok/root/config/extensions/ /home/gitpod/.gitpod-code/extensions
